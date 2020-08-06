f54928c163f2244007ada315023596eafd84aa18
aashimgarg <18bcs001@smvdu.ac.in>
2020-08-06 20:30:18 +0530

    added recomended videos :fire:

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import './RecommendedVideos.css'
 
-const RecommendedVideos =() => {
+import VideoCard from './VideoCard';
+const RecommendedVideos = () => {
     return (
         <div className="recomendedVideos">
-           <h1>Recomended videos</h1>
+            <h2>Recomended</h2>
+            <div className="recomendedVideos__videoCard">
+                <VideoCard
+                    title="Hello From Youtube | This is me from New York San Franciso | Live"
+                    views="2.3M views"
+                    timestamp="4 days ago"
+                    channelImage="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                    channel="Aashim Garg"
+                    image="https://static.toiimg.com/photo/72975551.cms" />
+
+                <VideoCard
+                    title="Hello From Facebook | This is me from New York San Franciso | Live"
+                    views="2.4M views"
+                    timestamp="4 days ago"
+                    channelImage="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"                    channel="Aashim Garg"
+                    image="https://cdn.eso.org/images/large/eso1322a.jpg" />
+
+                <VideoCard
+                    title="Hello From Youtube | This is me from New York San Franciso | Live"
+                    views="2.3M views"
+                    timestamp="4 days ago"
+                    channelImage="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                    channel="Aashim Garg"
+                    image="https://static.toiimg.com/photo/72975551.cms" />
+
+                <VideoCard
+                    title="Hello From Facebook | This is me from New York San Franciso | Live"
+                    views="2.4M views"
+                    timestamp="4 days ago"
+                    channelImage="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"                    channel="Aashim Garg"
+                    image="https://cdn.eso.org/images/large/eso1322a.jpg" />
+
+                <VideoCard
+                    title="Hello From Youtube | This is me from New York San Franciso | Live"
+                    views="2.3M views"
+                    timestamp="4 days ago"
+                    channelImage="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                    channel="Aashim Garg"
+                    image="https://static.toiimg.com/photo/72975551.cms" />
+
+                <VideoCard
+                    title="Hello From Facebook | This is me from New York San Franciso | Live"
+                    views="2.4M views"
+                    timestamp="4 days ago"
+                    channelImage="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"                    channel="Aashim Garg"
+                    image="https://cdn.eso.org/images/large/eso1322a.jpg" />
+
+
+
+            </div>
         </div>
     )
-    
+
 }
 
 export default RecommendedVideos;
---
+++ src/VideoCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './VideoCard.css'
+import Avatar from '@material-ui/core/Avatar';
+
+const VideoCard = ({
+    image,
+    title,
+    timestamp,
+    channelImage,
+    channel,
+    views
+}) => {
+    return (
+        <div className="videoCard">
+            <img className="videoCard__thumbnail" src={image} />
+            <div className="videoCard__info">
+                <Avatar
+                    alt=" Aashim Garg"
+                    src={channelImage}
+                    className="videoCard__avatar"
+                />
+                <div className="videoCard__text">
+                <h4>{title}</h4>
+                <p>{channel}</p>
+                <p>{views} . {timestamp}</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default VideoCard;
